9d534c4937991a4c17599a2ef3ada73171a4caa3
name <username@email.com>
2018-06-26 12:50:52 +0530

    commit#1

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+//home  page text animation action
+$(function() {
+$(".typed").typed({
+  strings:["Web Design.","^1000 MEAN Stack Developer.","^1000 Android Developer.","^1000 App Developer."],
+  loop:true,
+  smartBackspace: true
+});
+});
