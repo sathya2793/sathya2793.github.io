80e57a0af71097890b46e0f00bbde8891e03ac3a
name <username@email.com>
2018-07-17 23:20:19 +0530

    changed to bootstrap

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,36 @@ $(".typed").typed({
   smartBackspace: true
 });
 });
+
+//smooth scroll
+$("#navbar ul li a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 1000, function(){
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+});
+
+//navbar bg color change
+$(window).scroll(function(){
+  if(($(this).scrollTop() > 0) & ($(this).scrollTop() <650))
+  {
+  $("#navbar").css({"opacity":"0"})
+    $("#navbar").css({"visibility":"hidden"})
+  }
+  else {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 650);
+    $("#navbar").css({"opacity":"1"});
+      $("#navbar").css({"visibility":"visible"})
+  }
+});
